4efad4c3b33b921241c6a3326edcd0a69ed88133
Robsnodgrass <robert.snodgrass@live.co.uk>
2025-07-13 14:17:35 +0100

    Update zego_express_platform_view_impl_web.dart

--- lib/src/impl/zego_express_platform_view_impl_web.dart
+++ lib/src/impl/zego_express_platform_view_impl_web.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:html';
+import 'package:js/js.dart';
 
-// ignore: undefined_prefixed_name
-@pragma('vm:entry-point')
-external dynamic get platformViewRegistry;
+@JS('window.flutterWeb.platformViewRegistry')
+external PlatformViewRegistry get platformViewRegistry;
+
+@JS()
+@anonymous
+class PlatformViewRegistry {
+  external void registerViewFactory(String viewTypeId, Function viewFactory);
+}
 
 class ZegoExpressPlatformViewImpl {
   static Widget? createPlatformView(Function(int viewID) onViewCreated, {Key? key}) {
     const String webcamPushElement = 'plugins.zego.im/zego_express_view';
 
-    // ignore: undefined_prefixed_name
-    // Use the JS interop to call the platformViewRegistry
-    // The ignore is needed because it's not defined in Dart normally
-    platformViewRegistry.registerViewFactory(webcamPushElement, (int id) {
+    platformViewRegistry.registerViewFactory(webcamPushElement, allowInterop((int id) {
       return DivElement()..id = "zego-view-$id";
-    });
+    }));
 
     return HtmlElementView(
       key: key,
